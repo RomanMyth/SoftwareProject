419eab0640e97f10ad3ae4ad3b7a295e56f89fc4
RomanMyth <romanmax05@gmail.com>
2022-11-21 12:21:50 -0500

    expanding on js

--- menu.js
+++ menu.js
@@ -2,10 +2,9 @@ $(document).ready(function(){
     $("#italian").click(function(){
         $("#overlay").css("visibility", "visible");
         $("#italian-info").css("visibility", "visible");
-        console.log("test");
     });
     $(".close").click(function(){
         $("#overlay").css("visibility", "hidden");
-        $("#italian-info").css("visibility", "hidden");
+        $(this.parentElement).css("visibility", "hidden");
     });
 });

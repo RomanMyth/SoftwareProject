d9a70663a9a28a7881021ac95ff6a02f47ef39cb
RomanMyth <romanmax05@gmail.com>
2022-11-21 13:43:39 -0500

    started menu and popup functions

--- menu.js
+++ menu.js
@@ -3,8 +3,20 @@ $(document).ready(function(){
         $("#overlay").css("visibility", "visible");
         $("#italian-info").css("visibility", "visible");
     });
+    $("#zuppa").click(function(){
+        $("#overlay").css("visibility", "visible");
+        $("#zuppa-info").css("visibility", "visible");
+    });
+
     $(".close").click(function(){
         $("#overlay").css("visibility", "hidden");
         $(this.parentElement.parentElement).css("visibility", "hidden");
+    });
+
+    $(".item").mouseover(function(){
+        $(this).children(':nth-child(2)').children(':nth-child(3)').css("visibility", "visible");
+    });
+    $(".item").mouseleave(function(){
+        $(this).children(':nth-child(2)').children(':nth-child(3)').css("visibility", "hidden");
     });
 });
